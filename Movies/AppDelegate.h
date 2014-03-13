555ca0f47533c7f0ea144f5176ff88e8a72caf3f
Thomas Ezan <thomas@readitlater.com>
2014-03-12 21:44:05 -0700

    Initial Commit

---
+++ Movies/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Movies
+//
+//  Created by Thomas Ezan on 3/12/14.
+//  Copyright (c) 2014 Thomas Ezan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
